9f179f20163279b2a662f6a5032b574543af723a
Matthijs Brobbel <m1brobbel@gmail.com>
2022-01-13 16:51:56 +0100

    Update googletest git_tag to main

--- CompileUnits/CompileUnits.cmake
+++ CompileUnits/CompileUnits.cmake
@@ -6,7 +6,7 @@ include(GoogleTest)
 enable_testing()
 FetchContent_Declare(googletest
   GIT_REPOSITORY  https://github.com/google/googletest.git
-  GIT_TAG         master
+  GIT_TAG         main
 )
 FetchContent_GetProperties(googletest)
 if(NOT googletest_POPULATED)
